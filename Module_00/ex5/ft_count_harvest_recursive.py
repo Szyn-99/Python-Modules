fd211ffb01119471b10d8d0f9438f7800e48f3d3
Szyn <szyn@kali.szyn>
2025-12-27 19:03:50 +0100

    module 00

---
+++ Module_00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,14 @@
+def recursion_helper(days, i):
+    if(days < i):
+        return
+    else:
+        print("Day ", i)
+    recursion_helper(days, i+1)
+
+
+
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+    i = 1
+    recursion_helper(days, i)
+
