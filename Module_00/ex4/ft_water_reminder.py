3e5480e8bc158593d1b2ecb96aea4550fe30caa7
Szyn <szyn@kali.szyn>
2025-12-27 19:10:40 +0100

    Module 00

--- Module_00/ex4/ft_water_reminder.py
+++ Module_00/ex4/ft_water_reminder.py
@@ -1,7 +1,6 @@
 def ft_water_reminder():
     flag = int(input("Days since last watering: "))
-    if(flag > 2):
+    if flag > 2:
         print("Water the plants!")
     else:
-        print ("Plants are fine")
-
+        print("Plants are fine")
--- Module_00/ex6/ft_garden_summary.py
+++ Module_00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
     n_garden = input("Enter garden name: ")
     nop_garden = int(input("Enter number of plants: "))
-    print("Garden: ",n_garden)
+    print("Garden: ", n_garden)
     print("Plants: ", nop_garden)
     print("Status: Growing well!")
