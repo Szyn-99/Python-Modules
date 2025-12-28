45da95e9250087651d663b716b4838db764b735a
Szyn <szyn@kali.szyn>
2025-12-29 00:08:45 +0100

    go

--- Module_01/ex3/ft_plant_factory.py
+++ Module_01/ex3/ft_plant_factory.py
@@ -1,27 +1,27 @@
 class Plant:
     def __init__(self, name, height, age):
         self.name = name
-        self._height = height
-        self._age = age
+        self.__height = height
+        self.__age = age
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, n_height):
         if n_height <= 0:
             print("Height can not be negative")
         else:
-            self._height = n_height
+            self.__height = n_height
 
     @property
     def age(self):
-        return self._age
+        return self.__age
 
     @age.setter
     def age(self, n_age):
         if n_age <= 0:
             print("age can not be negative")
         else:
-            self._age = n_age
+            self.__age = n_age
