9730cac896ff405704f7d88549bc83f931487d1d
Aymane El Halkouj <aymel-ha@e1r6p12.1337.ma>
2026-01-01 20:56:42 +0100

    go

--- Module_01/ex6/ft_garden_analytics.py
+++ Module_01/ex6/ft_garden_analytics.py
@@ -1,3 +1,27 @@
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def grow(self):
+        self.height += 1
+
+    def age_cm(self):
+        self.age += 1
+
+    def get_info(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+class FloweringPlant(Plant):
+    pass
 class GardenManager:
+    gardens_total = 0
+    gardens_owners = []
+    gardens_owners_stats = []
+    def add_plant(self, owner):
     class GardenStats:
+
+        pass
         
