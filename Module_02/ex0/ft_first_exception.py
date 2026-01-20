f9dbc2a229f67da5ecebe1dd27719d3479f31bd1
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-20 16:32:28 +0100

    fix

--- Module_02/ex0/ft_first_exception.py
+++ Module_02/ex0/ft_first_exception.py
@@ -1,4 +1,4 @@
-def check_temperature(temp_str: str) -> int | None:
+def check_temperature(temp_str: str) -> None:
     try:
         try:
             temp = int(temp_str)
@@ -10,8 +10,8 @@ def check_temperature(temp_str: str) -> int | None:
             raise ValueError(f"Error: {temp}°C is too cold for plants (min 0°C)")
         else :
             print(f"Temperature {temp}°C is perfect for plants!")
-    except Exception as Error:
-        print(Error)
+    except Exception as er:
+        print(er)
         return None
 
     
@@ -20,8 +20,8 @@ def test_temperature_input() -> None:
     print("Testing temperature: 25")
     check_temperature("25") 
     print()
-    print("Testing temperature: abc")
-    check_temperature("abc")
+    print("Testing temperature: ab")
+    check_temperature("ALOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
     print()
     print("Testing temperature: 100")
     check_temperature("100")
