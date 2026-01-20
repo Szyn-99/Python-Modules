36676802b0e46f1d4b79ed68dcb2214e05d72272
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-21 00:50:01 +0100

    python 02

--- Module_02/ex5/ft_garden_management.py
+++ Module_02/ex5/ft_garden_management.py
@@ -16,22 +16,22 @@ class SunLightHoursError(GardenError):
 class Plant:
     def __init__(self, plant_name: str, water_level: int, sunlight_hours: int):
         if plant_name is None or plant_name == "":
-            raise IsValidNameError("Error: Plant name cannot be empty!")
+            raise IsValidNameError("Plant name cannot be empty!")
         elif water_level > 10:
             raise WaterLevelError(
-                f"Error: Water level {water_level} is too high (max 10)"
+                f"Water level {water_level} is too high (max 10)"
             )
         elif water_level < 1:
             raise WaterLevelError(
-                f"Error: Water level {water_level} is too low (min 1)"
+                f"Water level {water_level} is too low (min 1)"
             )
         elif sunlight_hours < 2:
             raise SunLightHoursError(
-                f"Error: Sunlight hours {sunlight_hours} is too low (min 2)"
+                f"Sunlight hours {sunlight_hours} is too low (min 2)"
             )
         elif sunlight_hours > 12:
             raise SunLightHoursError(
-                f"Error: Sunlight hours {sunlight_hours} is too high (max 12)"
+                f"Sunlight hours {sunlight_hours} is too high (max 12)"
             )
         else:
             self.plant_name = plant_name
@@ -44,47 +44,57 @@ class Plant:
 
 class GardenManager:
     garden = []
-    @staticmethod
+    adding_flag = 1
+    water_flag = 1
+    adding_flag = 1
     def add_plant(plant_name: str, water_level: int, sunlight_hours: int) -> None:
-     try:
-        plant = Plant(plant_name, water_level, sunlight_hours)
-        GardenManager.garden += [plant]
-     except GardenError as e:
-        print(e)
+        
+        try:
+            if GardenManager.adding_flag == 1:
+                GardenManager.adding_flag = 0
+                print("Adding plants to garden...")
+            plant = Plant(plant_name, water_level, sunlight_hours)
+            GardenManager.garden += [plant]
+            print(f"Added {plant_name} successfully")
+        except GardenError as error_add:
+            print(f"Error adding plant: {error_add}!")
+            
+    add_plant = staticmethod(add_plant)
+    
+    def water_plant() -> None:
+        if GardenManager.water == 1:
+            GardenManager.water = 0
+            print("Opening watering system")
+            print("Watering plants...")
+        for plant in GardenManager.garden:
+            plant.water_level += 1
+            print(f"Watering {plant.plant_name} - success")
+            
+    water_plant = staticmethod(water_plant)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @staticmethod
     def show_plant():
         for plant in GardenManager.garden:
-            print(plant.plant_name)
-    def check_plant_health(plant: Plant) -> str:
-        try:
-            if plant_name is None or plant_name == "":
-                raise IsValidNameError("Error: Plant name cannot be empty!")
-            elif water_level > 10:
-                raise WaterLevelError(
-                    f"Error: Water level {water_level} is too high (max 10)"
-                )
-            elif water_level < 1:
-                raise WaterLevelError(
-                    f"Error: Water level {water_level} is too low (min 1)"
-                )
-            elif sunlight_hours < 2:
-                raise SunLightHoursError(
-                    f"Error: Sunlight hours {sunlight_hours} is too low (min 2)"
-                )
-            elif sunlight_hours > 12:
-                raise SunLightHoursError(
-                    f"Error: Sunlight hours {sunlight_hours} is too high (max 12)"
-                )
-            else:
-                return f"Plant '{plant_name}' is healthy!"
-        except GardenError as hadi9a:
-            print(hadi9a)
+            print(plant.plant_name, plant.water_level, plant.sunlight_hours)
+   
 
 
 
 def test():
     garden = GardenManager()
-    garden.add_plant("test", 0, 4)
+    garden.add_plant("khizo", 4, 4)
+    garden.add_plant("btata", 4, 4)
+    garden.add_plant("", 4, 4)
+    garde.water_plant
     garden.show_plant()
     print()
     
