b560fea36ccc899556ca0c2d76e4dccfc77fd8c7
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-20 17:21:25 +0100

    fix

--- Module_02/ex0/ft_first_exception.py
+++ Module_02/ex0/ft_first_exception.py
@@ -4,32 +4,46 @@ def check_temperature(temp_str: str) -> None:
             temp = int(temp_str)
         except ValueError:
             raise ValueError(f"Error: '{temp_str}' is not a valid number")
-        if(temp > 40):
-            raise ValueError(f"Error: {temp}°C is too hot for plants (max 40°C)")
-        elif temp < 0:
+        if temp < 0:
             raise ValueError(f"Error: {temp}°C is too cold for plants (min 0°C)")
-        else :
+        elif temp > 40:
+            raise ValueError(f"Error: {temp}°C is too hot for plants (max 40°C)")
+        else:
             print(f"Temperature {temp}°C is perfect for plants!")
-    except Exception as er:
-        print(er)
+            return None
+    except Exception as alo:
+        print(alo)
         return None
 
-    
+
 def test_temperature_input() -> None:
-    print("=== Garden Temperature Checker ===")   
+    print("=== Garden Temperature Checker ===")
     print("Testing temperature: 25")
-    check_temperature("25") 
+    try:
+        check_temperature("25")
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
     print()
-    print("Testing temperature: ab")
-    check_temperature("ALOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    print("Testing temperature: abc")
+    try:
+        check_temperature("abc")
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
     print()
     print("Testing temperature: 100")
-    check_temperature("100")
+    try:
+        check_temperature("100")
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
     print()
     print("Testing temperature: -50")
-    check_temperature("-50")
+    try:
+        check_temperature("-50")
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
     print()
     print("All tests completed - program didn't crash!")
-    
+
+
 if __name__ == "__main__":
     test_temperature_input()
