3d0451bfd5b4ada035cc44c0cccc64ca7f2e4ab7
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-20 22:06:51 +0100

    python 02

--- Module_02/ex3/ft_finally_block.py
+++ Module_02/ex3/ft_finally_block.py
@@ -1,2 +1,40 @@
-def water_plants(plant_list):
+class InvalidPlantError(Exception):
     pass
+
+def water_plants(plant_list):
+    print("Opening watering system")
+    try:
+        for each in plant_list:
+            if each == None or each == "None":
+                raise InvalidPlantError(f"Error: Cannot water {each} - invalid plant!")
+            print(f"Watering {each}")
+        print("Watering completed successfully!")
+    except InvalidPlantError as plant:
+        print(plant)
+    finally:
+        print("Closing watering system (cleanup)")
+
+
+def  test_watering_system() -> None:
+    print("=== Garden Watering System ===")
+    
+    print("Testing normal watering...")
+    try:
+        water_plants(["tomato", "lettuce", "carrots"])
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
+    print()
+    
+    print("Testing with error...")
+    try:
+        water_plants(["tomato", None])
+    except Exception as subject_says_No_Crash:
+        print(f"Subject Requirement: {subject_says_No_Crash}")
+    print()
+    
+    print("Cleanup always happens, even with errors!")
+
+
+if __name__ == "__main__":
+    test_watering_system()
+
