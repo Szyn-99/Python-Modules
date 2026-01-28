227795155049259e798fc837ba4462a4717dcde7
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-28 10:29:45 +0100

    apah

--- Module_03/ex2/ft_coordinate_system.py
+++ Module_03/ex2/ft_coordinate_system.py
@@ -1,35 +1,39 @@
 import sys
 import math
 
-# def parsing_input(position: list) -> tuple:
-#     i = len(sys.argv[1:])
-#     if i == 3:
-#         Coordinate = tuple(position[1:])
-#     elif i > 3:
-#         raise ValueError("Too many arguments")
-#     elif i < 3:
-#         raise ValueError("Not enough arguments")
-#     return Coordinate
-            
-  
-def parsing_input_2(position: list) -> tuple:
+
+def get_len(argvs: list) -> int:
+    i = 0
+    for arg in argvs:
+        i += 1
+    return i
+
+def parsing_input_2(argvs: list) -> tuple:
     try:
-        cords = [int(i) for i in position]
+        length = get_len(argvs) - 1
+        if(length == 1):
+            cords = [int(i) for i in argvs[1].split(',')]
+        elif length == 3:
+            cords = [int(i) for i in argvs[1:]]
+        elif length > 3:
+            raise ValueError("Too many arguments")
+        elif length < 3:
+            raise ValueError("Not enough arguments") 
         Coordinate = tuple(cords)
-    except ValueError:
-        raise ValueError("Invalid data, only floats/ints acceptable")
+    except Exception:
+        raise
     return Coordinate
 
 def ft_coordinate_system() -> None:
     print("=== Game Coordinate System ===")
     try:
-        position = parsing_input_2(sys.argv[1].split(','))
+        position = parsing_input_2(sys.argv)
         distance = math.sqrt(position[0]**2 + position[1]**2 + position[2]**2)
         print(f"Position created: {position}")
         print(f"Distance between (0, 0, 0) and {position}: {distance:.2f}")
-        
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error parsing coordinates: {e}")
+        print(f"Error details - Type: {e.__class__.__name__}, Args: (\"{e}\")")
 
 if __name__ == "__main__":
     ft_coordinate_system()
