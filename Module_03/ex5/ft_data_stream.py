58e7e63f6f92773926ff32dfc92242021afcb88c
Aymane El Halkouj <aymel-ha@e2r6p7.1337.ma>
2026-01-29 15:59:21 +0100

    apah

--- Module_03/ex5/ft_data_stream.py
+++ Module_03/ex5/ft_data_stream.py
@@ -1,29 +1,59 @@
-def getting_data(num_events=100):
-    players = ("Alice", "Bob", "Charlie", "Diana")
-    events = ("killed monster", "found treasure", "scored", "leveled up")
-    levels = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
-    
+def events():
+    yield "killed monster"
+    yield "found treasure"
+    yield "scored"
+    yield "leveled up"
 
-def ft_data_stream():
+def players():
+    yield "Alice"
+    yield "Bob"
+    yield "Charlie"
+    yield "Diana"
+
+def levels(num):
+    for level in range(1, num + 1):
+        yield level
+
+def ft_data_stream(num):
     try:
         print("Starting data stream processing...")
-        players, events, levels = getting_data()
+        players_i = players()
+        events_i = events()
+        levels_i = levels(num)
+
         total_events = 0
         treasure_events = 0
         levelup_events = 0
         high_level_events = 0
-        for player in players:
-            for event in events:
-                if event == "found treasure":
-                    treasure_events += 1
-                elif event == "leveled up":
-                    levelup_events += 1
-                elif levels == 10:
-                    high_level_events += 1
-                total_events += 1
-                print(f"Event {total_events}: Player {player} (level 5) {event}")
+        event_counter = 0
+        while event_counter < num:
+            try:
+                player = next(players_i)
+            except StopIteration:
+                players_i = players()
+                player = next(players_i)
+            try:
+                event = next(events_i)
+            except StopIteration:
+                events_i = events()
+                event = next(events_i)
+            try:
+                level = next(levels_i)
+            except StopIteration:
+                levels_i = levels(num)
+                level = next(levels_i)
+                
+            event_counter += 1
+            if event == "found treasure":
+                treasure_events += 1
+            elif event == "leveled up":
+                levelup_events += 1
+            elif level == 10:
+                high_level_events += 1
+            total_events += 1
+            print(f"Event {total_events}: Player {player} (level {level}) {event}")
     except Exception as e:
         print(f"Finished with error: {e}")
         
 if __name__ == "__main__":
-    ft_data_stream()
+    ft_data_stream(50)
