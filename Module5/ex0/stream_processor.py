9b19fe20dbcb184e4a64cf8e55c08b1b2f7725c6
Aymane El Halkouj <aymel-ha@e2r8p1.1337.ma>
2026-02-11 19:31:04 +0100

    python 05

--- Module5/ex0/stream_processor.py
+++ Module5/ex0/stream_processor.py
@@ -1,51 +1,62 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Dict, Union, Optional
+from typing import Any
+
 
 class DataProcessor(ABC):
     def __init__(self) -> None:
         super().__init__()
+
     @abstractmethod
     def process(self, data: Any) -> str:
         pass
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         pass
+
     def format_output(self, result: str) -> str:
         return f"Output: {result}"
-    
+
+
 class NumericProcessor(DataProcessor):
     def __init__(self):
         super().__init__()
+
     def process(self, data: Any) -> str:
         try:
-            
+
             iterable = True
             error = False
             try:
                 iter(data)
             except Exception:
                 iterable = False
-            
+
             print("Initializing Numeric Processor...")
             print(f"Processing data: {data}")
             if iterable:
                 for i in data:
-                    if (i.__class__.__name__ == "int" or i.__class__.__name__ == "float") and i.__class__.__name__ != "bool":
+                    if (i.__class__.__name__ == "int" or
+                            i.__class__.__name__ == "float") and \
+                            i.__class__.__name__ != "bool":
                         continue
                     else:
                         error = True
             if not iterable:
-                if (data.__class__.__name__ == "int" or data.__class__.__name__ == "float") and data.__class__.__name__ != "bool":
+                if (data.__class__.__name__ == "int" or
+                        data.__class__.__name__ == "float") and \
+                        data.__class__.__name__ != "bool":
                     iterable = False
                 else:
                     error = True
             if error:
-                return f"[VALUE] ERROR level detected: a value is not numeric"
-            return f"Processed {len(data) if iterable else data} numeric values, sum={sum(data) if iterable else data}, avg={sum(data) / len(data) if iterable else data:.2f}"   
+                return "[VALUE] ERROR level detected: a value is not numeric"
+            return (f"Processed {len(data) if iterable else data} numeric "
+                    f"values, sum={sum(data) if iterable else data}, "
+                    f"avg={sum(data) / len(data) if iterable else data:.2f}")
         except Exception as e:
             return f"Error: {e}"
-            
-    
+
     def validate(self, data: Any) -> bool:
         iterable = True
         try:
@@ -54,13 +65,17 @@ class NumericProcessor(DataProcessor):
             iterable = False
         if iterable:
             for i in data:
-                if (i.__class__.__name__ == "int" or i.__class__.__name__ == "float") and i.__class__.__name__ != "bool":
+                if (i.__class__.__name__ == "int" or
+                        i.__class__.__name__ == "float") and \
+                        i.__class__.__name__ != "bool":
                     continue
                 else:
                     print("Validation: Data is not numeric")
                     return False
         else:
-            if (data.__class__.__name__ == "int" or data.__class__.__name__ == "float") and data.__class__.__name__ != "bool":
+            if (data.__class__.__name__ == "int" or
+                    data.__class__.__name__ == "float") and \
+                    data.__class__.__name__ != "bool":
                 iterable = False
             else:
                 print("Validation: Data is not numeric")
@@ -71,29 +86,34 @@ class NumericProcessor(DataProcessor):
     def format_output(self, result: str) -> str:
         print(super().format_output(result))
 
+
 class TextProcessor(DataProcessor):
     def __init__(self):
         super().__init__()
-    
+
     def process(self, data: Any) -> str:
         print("Initializing Text Processor...")
         print(f'Processing data: "{data}"')
         if data.__class__.__name__ == "str":
-            return f"Processed text: {len(data)} characters, {len(data.split(' '))} words"
+            return (f"Processed text: {len(data)} characters, "
+                    f"{len(data.split(' '))} words")
         return ""
+
     def validate(self, data: Any) -> bool:
         if data is not None and data.__class__.__name__ == "str":
             print("Validation: Text data verified")
             return True
         print("Validation: Text data is not verified")
         return False
+
     def format_output(self, result: str) -> str:
         print(super().format_output(result))
+
 
 class LogProcessor(DataProcessor):
     def __init__(self):
         super().__init__()
-    
+
     def process(self, data: Any) -> str:
         print("Initializing Log Processor...")
         print(f'Processing data: "{data}"')
@@ -104,17 +124,19 @@ class LogProcessor(DataProcessor):
             else:
                 error_type = "INFO"
             return f'[{error_type}] {error} level detected: {error_details}'
-    
+
         return "Invalid log format"
+
     def validate(self, data: Any) -> bool:
-        if data is not None and data.__class__.__name__ == "str" and ":" in data:
+        if (data is not None and data.__class__.__name__ == "str" and
+                ":" in data):
             print("Validation: Log entry is verified")
             return True
         print("Validation: Log entry is not verified")
         return False
-    
+
     def format_output(self, result: str) -> str:
-        print(super().format_output(result))    
+        print(super().format_output(result))
 
 
 def log():
@@ -123,6 +145,8 @@ def log():
     result = test.process(data)
     test.validate(data)
     test.format_output(result)
+
+
 def literal():
     data = "Szyn"
     test = TextProcessor()
@@ -131,6 +155,8 @@ def literal():
     test.format_output(result)
     print()
     print()
+
+
 def numeric() -> None:
     data = True
     test = NumericProcessor()
@@ -139,6 +165,8 @@ def numeric() -> None:
     test.format_output(result)
     print()
     print()
+
+
 if __name__ == "__main__":
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
     numeric()
@@ -147,7 +175,7 @@ if __name__ == "__main__":
     print("=== Polymorphic Processing Demo ===")
     print("\nProcessing multiple data types through same interface...")
     processors = (NumericProcessor(), TextProcessor(), LogProcessor(),)
-    data = [[2, 2, 2],"Vasto Lorde","INFO: System ready"]
+    data = [[2, 2, 2], "Vasto Lorde", "INFO: System ready"]
 
-    for i , (proc, value) in enumerate(zip(processors, data)):
+    for i, (proc, value) in enumerate(zip(processors, data)):
         print(f"Result {i}: {proc.process(value)}")
--- Module5/ex1/data_stream.py
+++ Module5/ex1/data_stream.py
@@ -1,2 +1,34 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional
+
+
+
+class DataStream(ABC):
+    def __init__(self, stream_id):
+        self.stream_id = stream_id
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+    def filter_data(self, data_batch: List[Any], criteria: Optional[str] = None) -> List[Any]:
+        pass
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+class SensorStream(DataStream):
+    def __init__(self, stream_id):
+        super().__init__(stream_id)
+        self.type = "Environmental Data"
+    def process_batch(self, data_batch: List[Any]) -> str:
+        readings = 0
+        print(f"Processing sensor batch: {data_batch}")
+        if data_batch:
+            lis = [(t.split(":")[0], t.split(":")[1]) for t in data_batch]
+            for 
+a = SensorStream("SENSOR")
+a.process_batch(["temp:22", "humidity:11", "alo:3"])
+            
+
+class TransactionStream(DataStream):
+    pass
+class EventStream(DataStream):
+    pass
