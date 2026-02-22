14312a67e93f2432edf119123f2016f8e287b03a
Aymane El Halkouj <aymel-ha@e1r7p14.1337.ma>
2026-02-22 18:20:25 +0000

    python module 7

---
+++ Module7/ex1/Deck.py
@@ -0,0 +1,27 @@
+from ex0.Card import Card
+import random
+class Deck:
+    def __init__(self):
+        self.cards = []
+    def add_card(self, card: Card) -> None:
+        self.cards.append(card)
+    def remove_card(self, card_name: str) -> bool:
+        for card in self.cards:
+            if card.name == card_name:
+                self.cards.remove(card)
+                return True
+        return False
+    def shuffle(self) -> None:
+        random.shuffle(self.cards)
+    def draw_card(self) -> Card:
+        if self.cards:
+            return self.cards.pop(0)
+    def get_deck_stats(self) -> dict:
+        deck_stats = {'total_cards': len(self.cards),
+                      'creatures': sum(1 for card in self.cards if card.type == "Creature"),
+                      'spells': sum(1 for card in self.cards if card.type == "Spell"),
+                      'artifacts': sum(1 for card in self.cards if card.type == "Artifact")}
+        unique_cards = set(card for card in self.cards)
+        average_cost = sum(card.cost for card in unique_cards) / len(unique_cards) if unique_cards else 0
+        deck_stats['average_cost'] = average_cost
+        return deck_stats
